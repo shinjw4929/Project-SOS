11a0545edb2d66d5d9dfbf5086408c612e3ebd0c
leedongju <114165018+leeedongjoo@users.noreply.github.com>
2026-01-22 09:29:28 +0900

    Start Exit 제목까지

---
+++ Assets/Scripts/GameOnOff/GameStartController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameStartController : MonoBehaviour
+{
+    [SerializeField] private GameObject menuRoot;
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+        if (menuRoot != null) menuRoot.SetActive(true);
+    }
+
+    public void OnClickStart()
+    {
+        Time.timeScale = 1f;
+        if (menuRoot != null) menuRoot.SetActive(false);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
